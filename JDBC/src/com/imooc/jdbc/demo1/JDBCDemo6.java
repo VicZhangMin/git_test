d523294f3e58d6d3b8ef839e3b3a784b58798f66
Vic <viczhangmin@hotmail.com>
2021-06-13 15:43:27 +0800

    version5

--- JDBC/src/com/imooc/jdbc/demo1/JDBCDemo6.java
+++ JDBC/src/com/imooc/jdbc/demo1/JDBCDemo6.java
@@ -122,7 +122,7 @@ public class JDBCDemo6 {
 			// 执行SQL:
 			int num = pstmt.executeUpdate();
 			if(num > 0){
-				System.out.println("修改成功！version4");
+				System.out.println("修改成功！version5");
 			}
 		}catch(Exception e){
 			e.printStackTrace();
